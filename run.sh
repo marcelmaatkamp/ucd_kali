e094933519ab02d8c66ba95217616d4ffd533a58
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-01-31 13:39:35 +0100

    terminator added

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name ucd-kali -ti --privileged --net=host marcelmaatkamp/ucd-kali:latest 
+docker run --rm --name ucd-kali -ti --privileged --net=host marcelmaatkamp/ucd-kali:latest 
